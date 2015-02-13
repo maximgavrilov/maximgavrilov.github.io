5dab52a3d6d57cf10e6383a364a9359673eaba25
Maxim Gavrilov <max@max-imac-r.local>
2015-02-13 18:19:23 +0300

    fd

--- flappy.js
+++ flappy.js
@@ -1,46 +1,61 @@
+'use strict'
+
 function init() {	
 	var hdpi = window.devicePixelRatio || 1;
+	var VERSION = 6;
 	var WIDTH = 150 * hdpi, HEIGHT = 200 * hdpi;
 	var SPEED = 100;
 
-	function TestState(game) {
-		this.preload = function () {
-			var birdData = {
-				'frames' : [
-				{ 'frame' : { 'x' : 0, 'y' : 0, 'w' : 17, 'h' : 12} },
-				{ 'frame' : { 'x' : 17, 'y' : 0, 'w' : 17, 'h' : 12} },
-				{ 'frame' : { 'x' : 2 * 17, 'y' : 0, 'w' : 17, 'h' : 12}}
-				]
-			};
+	function create_back(game) {
+		var GR = 24 * hdpi;
 
-			game.load.atlas('bird', 'f_bird.png', null, birdData);
+		var ground = game.add.graphics(0, HEIGHT - GR);
+		ground.beginFill(0xf5dab5);
+		ground.drawRect(0, 0, WIDTH, GR);
+		ground.endFill();
+
+		var down = game.add.tileSprite(0, HEIGHT - GR, 300, 26, 'down');
+		down.scale = new PIXI.Point(hdpi, hdpi);
+		down.autoScroll(-SPEED, 0);
+		down.smoothed = false;
+
+		var bg = game.add.image(0, HEIGHT - GR - 200 * hdpi, 'bg');
+		bg.scale = new PIXI.Point(hdpi, hdpi);
+		bg.smoothed = false;
+	}
+
+	function create_bird(game) {
+		var bird = game.add.sprite(100, 100, 'bird');
+		bird.animations.add('fly', [0, 1, 2, 1], 6, true);
+		bird.animations.play('fly');
+		bird.smoothed = false;
+		bird.scale = new PIXI.Point(hdpi, hdpi);
+	}
+
+	function PreloadState(game) {
+		this.preload = function () {
+			game.load.spritesheet('bird', 'n_bird.png', 17, 12, 3);
 			game.load.image('down', 'f_down.png');
 			game.load.image('bg', 'f_bg.png');
 			game.load.image('wall', 'f_wall.png');
-		},
-
+			game.load.image('btn_play','btn_play.png');
+		}
 		this.create = function () {
-			var GR = 24 * hdpi;
+			game.state.start('menu');
+		}
+	}
 
-			var ground = game.add.graphics(0, HEIGHT - GR);
-			ground.beginFill(0xf5dab5);
-			ground.drawRect(0, 0, WIDTH, GR);
-			ground.endFill();
+	function MenuState(game) {
+		this.create = function () {
+			create_back(game);
+			create_bird(game);
 
-			var down = game.add.tileSprite(0, HEIGHT - GR, 300, 26, 'down');
-			down.scale = new PIXI.Point(hdpi, hdpi);
-			down.autoScroll(-SPEED, 0);
-			down.smoothed = false;
+			var play = game.add.button(game.world.centerX - 58 * hdpi /2, 150, 'btn_play', function () {				
+				game.state.start('game');
+			});
+			play.smoothed = false;
+			play.scale = new PIXI.Point(hdpi, hdpi);
 
-			var bg = game.add.image(0, HEIGHT - GR - 200 * hdpi, 'bg');
-			bg.scale = new PIXI.Point(hdpi, hdpi);
-			bg.smoothed = false;
-
-			var bird = game.add.sprite(100, 100, 'bird');
-			bird.animations.add('fly', [0, 1, 2, 1], 6, true);
-			bird.animations.play('fly');
-			bird.smoothed = false;
-			bird.scale = new PIXI.Point(hdpi, hdpi);
 
 			// for (var i = 0; i < WIDTH; i += 17 * hdpi) {
 			// 	for (var j = 0; j < HEIGHT - GR - 12 * hdpi; j += 12 * hdpi) {
@@ -54,6 +69,12 @@ function init() {
 		}
 	}
 
+	function GameState(game) {
+		this.create = function () {
+			create_back(game);
+		}
+	}
+
 	function FPSPlugin(game) {
 		var fpsSpan = document.getElementById('fps');
 		var gameDiv = document.getElementById('game');
@@ -61,7 +82,7 @@ function init() {
 		this.render = function () {
 			var r = (game.renderType == Phaser.WEBGL) ? "WebGL" : "Canvas";
 			if (fpsSpan) {
-				fpsSpan.innerHTML = '' + game.time.fps + ' ' + gameDiv.clientWidth + 'x' + gameDiv.clientHeight + ' ' + window.devicePixelRatio + ' ' + r + ' 5';
+				fpsSpan.innerHTML = '' + game.time.fps + ' ' + gameDiv.clientWidth + 'x' + gameDiv.clientHeight + ' ' + window.devicePixelRatio + ' ' + r + ' ' + VERSION;
 			}
 		}
 	}
@@ -87,6 +108,8 @@ function init() {
 			game.plugins.add(FPSPlugin);
 		});
 
-		game.state.add('test', TestState, true);
+		game.state.add('preload', PreloadState, true);
+		game.state.add('menu', MenuState);
+		game.state.add('game', GameState);
 	})()
 }
