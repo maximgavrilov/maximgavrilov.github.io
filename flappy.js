d3750b63bdbd0dd722f7bac4c712ea6862857aaf
Maxim Gavrilov <max@max-imac-r.local>
2015-03-17 13:41:12 +0300

    fd

--- flappy.js
+++ flappy.js
@@ -469,7 +469,7 @@ function init() {
         this.add(result);
 
         var buttons = game.add.group();
-        buttons.add(add_button(game, 38, 154, 'btn_menu', function () {
+        buttons.add(add_button(game, 38, 137, 'btn_menu', function () {
             hide_to_state(game, function () { game.state.start('menu'); });
         }));
         this.add(buttons);
